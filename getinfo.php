520affae822c50f9abfee8e30614a4e1301ca723
Pete R <ptr616@gmail.com>
2023-04-07 15:30:30 -0400

    added seervice test via ui

--- getinfo.php
+++ getinfo.php
@@ -3,6 +3,13 @@
         You entered the following account number: 
         <?php
         echo $_POST["AcntNum"];
+        $url= "https://ptrbankapp.azurewebsites.net/svclookup.php?acntnumb=55467";
+        $response = fopen($url, r);
+        echo "<br>response: ";
+        echo $response;
         ?>
+        <br><br>
+        <a id="enter" href="BankMain.php">Enter Test App</a><br><br>
     </body>
+
 </html>
