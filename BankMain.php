8362d90c9fc7651612b49ebda3d963f39aebbaf9
Pete R <ptr616@gmail.com>
2023-04-07 14:17:09 -0400

    working on php

--- BankMain.php
+++ BankMain.php
@@ -6,7 +6,7 @@
 <p>Authorized Use Only</p>
 <p>This website is for testing purposes</p>
 <br><br>
-<a id="enter" href="index.html">Exit Bank App</a><br><br>
+<a id="exit" href="index.html">Exit Bank App</a><br><br>
 
 
 </body>
--- testphp.php
+++ testphp.php
@@ -4,8 +4,9 @@
 <h1>PHP Test Page</h1>
 <p>Authorized Use Only</p>
 <php?
-echo "PHP is working!!";
+echo " PHP is working!!";
+print " testing print statement";
 ?>
-
+<a id="exit" href="index.html">Exit Test Page</a><br><br>
 </body>
 </html>
