d5ba5ca2c6fa2f0fcaa21087153cd48984ad781e
Pete R <ptr616@gmail.com>
2023-04-07 18:53:15 -0400

    adding test file to see about 404 issue

---
+++ testnew.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>PHP Test Page</h1>
+<p>Authorized Use Only</p>
+<?php
+echo " PHP is working!!";<br>
+print " testing print statement";<br><br>
+?>
+<a id="exit" href="index.html">Exit Test Page</a><br><br>
+</body>
+</html>
