38feb7135f747c4e91252453881de21475d1776f
Pete R <ptr616@gmail.com>
2023-04-07 22:32:21 -0400

    fixing php errors

--- createtable.php
+++ createtable.php
@@ -2,13 +2,13 @@
 $servername = "ptrbankapp2-server.mysql.database.azure.com";
 $username = "srkxelcnue";
 $password = "31VNTO0TBI673202$";
-$dbname = "ptrbankapp2-database"
+$dbname = "ptrbankapp2-database";
 $sql = "CREATE TABLE UserData (
   ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
   FNAME VARCHAR(30),
   CHKBAL  DECIMAL(8,2),
   CCBAL DECIMAL(8,2)
-  )"
+  )";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
