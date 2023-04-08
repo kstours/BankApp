85d6d57e4dc8413c00817e9650d1980adeebdb33
Pete R <ptr616@gmail.com>
2023-04-08 19:03:05 -0400

    button test

--- getinfo.php
+++ getinfo.php
@@ -12,6 +12,7 @@
         <br><br>
         <p>Available Account Actions:<p><br><br>
         <a id="balance" href="checkbal.php">Check Balances</a><br><br>
+        <button onclick="document.location='checkbal.php'">Check Balances</button><br>
         <a id="paycc" href="paycc.php">Pay Credit Card</a><br><br>
         <a id="enter" href="BankMain.php">Log Out</a><br><br>
     </body>
