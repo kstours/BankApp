1ed50af7676622e903ea7a9bba0e7a6774696bb2
Pete R <ptr616@gmail.com>
2024-04-03 19:09:46 -0400

    filldb changes

--- filldatabase.php
+++ filldatabase.php
@@ -13,18 +13,22 @@ echo $password ;
 echo $dbname;
 echo " ";
 //Establishes the connection
-$conn = mysqli_init();
-mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306);
-if (mysqli_connect_errno($conn)) {
-echo 'Failed to connect to MySQL: '.mysqli_connect_error();
+try  {
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306);
+} catch (Exception $e)  {
+    echo 'Failed to connect to MySQL: '.$e->getMessage();
 }
 
 //Create an Insert prepared statement and run it
-$stmt = "insert into userdata (ID, FNAME, CHKBAL, CCBAL) values (24200, 'King, Stephen' ,100.00,100.00)";
-mysqli_stmt_execute($stmt);
-printf($stmt);
-mysqli_stmt_close($stmt);
-
+try {
+    $stmt = "insert into userdata (ID, FNAME, CHKBAL, CCBAL) values (24200, 'King, Stephen' ,100.00,100.00)";
+    mysqli_stmt_execute($stmt);
+    printf($stmt);
+    mysqli_stmt_close($stmt);
+}catch (Exception $e)  {
+    echo 'Failed to insert to MySQL: '.$e->getMessage();
+}
 // Close the connection
 mysqli_close($conn);
 ?>
