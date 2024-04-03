a80ce34e08558da1a444c317c7f0921f9ae5f3be
Pete R <ptr616@gmail.com>
2024-04-03 18:01:25 -0400

    new page and more output

--- testsql2.php
+++ testsql2.php
@@ -1,35 +1,30 @@
-<!DOCTYPE html>
-<html>
-<body>
 <h1>Application Status</h1>
 <p>Authorized Use Only</p><br><br>
 <p>Web site is up<p>
 <?php
-$servername = "jwu-bankapp-server.mysql.database.azure.com";
+$host = "jwu-bankapp-server.mysql.database.azure.com";
 $username = "ogvxwnfife";
 $password = "tX24DD$ss468";
-$dbname = "jwu-bankapp-database";
+$db_name = "jwu-bankapp-database";
 
 echo " PHP is working";
 
+//Establishes the connection
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'jwu-bankapp-server.mysql.database.azure.com', 'ogvxwnfife', 'tX24DD$ss468', 'jwu-bankapp-database', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
-    echo " SQL Error.   ";
- //   die('Failed to connect to MySQL: '.mysqli_connect_error());
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 //Run the Select query
 printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM userdata');
+$res = mysqli_query($conn, 'SELECT * FROM Products');
 while ($row = mysqli_fetch_assoc($res)) {
 var_dump($row);
 }
 
 //Close the connection
 mysqli_close($conn);
-
 
 ?>
 <br><br><a id="querytest" href="testquery.php">Run Test Query</a><br><br>
