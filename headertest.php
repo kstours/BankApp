34faab55960cfbda691a37db72f04a235eef8ac7
Pete R <ptr616@gmail.com>
2024-04-06 11:15:40 -0400

    fix to headertest

--- headertest.php
+++ headertest.php
@@ -1,6 +1,15 @@
+<body>
+<html>
+<p>Some HTML text</p>
+
 <?php
+echo "here is some php before the header";
+
 include header.php;
 
 echo "here is some more php after the header";
 
 ?>
+
+</body>
+</html>
